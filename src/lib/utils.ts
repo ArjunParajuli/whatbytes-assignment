371ee51588572efbcceeb0e55d6e424248717c96
Arjun-Parajuli <arjun.parajuli@gamearena.tech>
2025-07-12 15:24:53 +0530

    Replaced any with unknown

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,11 +21,11 @@ export function generateId(): string {
   return Math.random().toString(36).substr(2, 9);
 }
 
-export function debounce<T extends (...args: any[]) => any>(
+export function debounce<T extends (...args: unknown[]) => void>(
   func: T,
   wait: number
 ): (...args: Parameters<T>) => void {
-  let timeout: NodeJS.Timeout;
+  let timeout: ReturnType<typeof setTimeout>;
   return (...args: Parameters<T>) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), wait);
